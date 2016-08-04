b8f78939a9138758202ee443b26587b85a71199a
Wing Ming Chan <chanw@upstate.edu>
2016-08-04 14:43:22 -0400

    Initial commit

--- edu/upstate/chanw/db/CascadeDB.java
+++ edu/upstate/chanw/db/CascadeDB.java
@@ -12,71 +12,71 @@ Therefore, there are only a few methods left.
 
 public class CascadeDB
 {
-	public CascadeDB() throws SQLException
-	{
-		try
-		{
-			conn            = CascadeDBReadConnection.getConnection();
-			stat            = conn.createStatement( 
-							      ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
-			result          = null;
-		}
-		catch( SQLException e )
-		{
-			System.out.println( "Printing from CascadeDBReadConnection " + e.getMessage() );
-			throw e;
-		}
-	}
-	
-	@Override
-	public void finalize() throws Exception
-	{
-		stat.close();
-		conn.close();
-		result          = null;
-	}
+    public CascadeDB() throws SQLException
+    {
+        try
+        {
+            conn            = CascadeDBReadConnection.getConnection();
+            stat            = conn.createStatement( 
+                                  ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY );
+            result          = null;
+        }
+        catch( SQLException e )
+        {
+            System.out.println( "Printing from CascadeDBReadConnection " + e.getMessage() );
+            throw e;
+        }
+    }
+    
+    @Override
+    public void finalize() throws Exception
+    {
+        stat.close();
+        conn.close();
+        result          = null;
+    }
 
-	public Document getDataAsDocument( String sql ) throws Exception
-	{
-		result = stat.executeQuery( sql );
-		String xmlString = "<records>";
-		
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		
-		ResultSetMetaData rsmd = result.getMetaData();
-		int columnsNumber = rsmd.getColumnCount();
-		
-		while( result.next() )
-		{
-			String temp = "<record>";
-			
-			for( int i = 1; i <= columnsNumber; i++ )
-			{
-				String temp1 = "<item>" + result.getString( i ) + "</item>";
-				temp += temp1;
-			}
-			
-			temp += "</record>";
-			xmlString += temp;
-		}
-		
-		xmlString += "</records>";
-		
-		InputSource is = new InputSource( new java.io.StringReader( xmlString ) );
-		Document dom   = builder.parse( is );
-		
-		return dom;
-	}
+    public Document getDataAsDocument( String sql ) throws Exception
+    {
+        result = stat.executeQuery( sql );
+        String xmlString = "<records>";
+        
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        
+        ResultSetMetaData rsmd = result.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        
+        while( result.next() )
+        {
+            String temp = "<record>";
+            
+            for( int i = 1; i <= columnsNumber; i++ )
+            {
+                String temp1 = "<item>" + result.getString( i ) + "</item>";
+                temp += temp1;
+            }
+            
+            temp += "</record>";
+            xmlString += temp;
+        }
+        
+        xmlString += "</records>";
+        
+        InputSource is = new InputSource( new java.io.StringReader( xmlString ) );
+        Document dom   = builder.parse( is );
+        
+        return dom;
+    }
 
-	public ResultSet getResultSet( String sql ) throws SQLException
-	{
-		result = null;
-		result = stat.executeQuery( sql );
-		return result;
-	}
-	
-	private Connection conn;
-	private Statement stat;
-	private ResultSet result;
+    public ResultSet getResultSet( String sql ) throws SQLException
+    {
+        result = null;
+        result = stat.executeQuery( sql );
+        return result;
+    }
+    
+    private Connection conn;
+    private Statement stat;
+    private ResultSet result;
 }
