b13f85cdec9cc0cbdd5c3087b8667d2018328cbe
Wing Ming Chan <chanw@upstate.edu>
2016-08-04 13:38:10 -0400

    Initial commit

--- edu/upstate/chanw/db/CascadeDBReadConnection.java
+++ edu/upstate/chanw/db/CascadeDBReadConnection.java
@@ -8,34 +8,35 @@ import java.sql.*;
 
 public class CascadeDBReadConnection
 {
-	public static Connection getConnection() throws SQLException
-	{
-		String url      = "";
-		String username = "";
-		String password = "";
-		
-		try
-		{
-			String contextPath = System.getProperty( "catalina.home" ) + "/conf/context.xml";
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			InputSource inputSource = new InputSource( contextPath );
-	
-			username = xpath.evaluate( "/Context/Resource/@username", inputSource );
-			password = xpath.evaluate( "/Context/Resource/@password", inputSource );
-			url      = xpath.evaluate( "/Context/Resource/@url", inputSource );
-			String drivers  = xpath.evaluate( "/Context/Resource/@driverClassName", inputSource );
-	      
-			if( drivers != null ) 
-				System.setProperty( "jdbc.drivers", drivers );
-		}
-		catch( XPathExpressionException e )
-		{
-			// if failed, hard-code the information here
-			url      = "";
-			username = "";
-			password = "";
-		}
-		
-		return DriverManager.getConnection( url, username, password );
-	}
+    public static Connection getConnection() throws SQLException
+    {
+        String url      = "";
+        String username = "";
+        String password = "";
+        String drivers  = "";
+        
+        try
+        {
+            String contextPath = System.getProperty( "catalina.home" ) + "/conf/context.xml";
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            InputSource inputSource = new InputSource( contextPath );
+    
+            username = xpath.evaluate( "/Context/Resource/@username", inputSource );
+            password = xpath.evaluate( "/Context/Resource/@password", inputSource );
+            url      = xpath.evaluate( "/Context/Resource/@url", inputSource );
+            drivers  = xpath.evaluate( "/Context/Resource/@driverClassName", inputSource );
+        }
+        catch( XPathExpressionException e )
+        {
+            // if failed, hard-code the information here
+            url      = "";
+            username = "";
+            password = "";
+            drivers  = "";
+        }
+        
+        System.setProperty( "jdbc.drivers", drivers );
+        
+        return DriverManager.getConnection( url, username, password );
+    }
 }
